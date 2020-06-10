f54c3d557377ba86232ff7b0d7e159d5c9790293
Miguel Angel Tomairo <miguel.tomairo@mdp.com.pe>
2020-06-10 04:14:51 -0500

    initial commit

---
+++ Headlines/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Headlines
+//
+//  Created by Mohammad Azam on 11/19/17.
+//  Copyright © 2017 Mohammad Azam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
